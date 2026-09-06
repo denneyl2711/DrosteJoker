cd347c9b3f684354d3dd9dc4f2670b2d7c5f2a1e
denneyl2711 <lukedenneymn@gmail.com>
2026-09-05 22:26:13 -0500

    Update verbiage and text coloring.

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -4,9 +4,9 @@ return {
             j_droste_droste = {
                 name = 'Droste Joker',
                 text = {
-                    'When sold, {C:green}#1# in #2# chance{}',
-                    'to spawn a copy of this Joker with',
-                    '{X:mult,C:white} X#3# {} Mult and decreased odds',
+                    'Sell this card for a',
+                    '{C:green}#1# in #2# chance{} to create a {C:attention}copy{}',
+                    'with {X:mult,C:white} X#3# {} Mult and decreased odds',
                     '{C:inactive} (Currently {X:mult,C:white}X#4# {C:inactive} Mult){}'
                 }
             }
